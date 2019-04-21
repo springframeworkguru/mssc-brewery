802ddcf011704581b1c2ceb40570b62fbcd4dacb
John Thompson <john@springframework.guru>
2019-04-21 16:32:38 -0400

    adding put example

--- src/main/java/guru/springframework/msscbrewery/services/BeerService.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerService.java
@@ -11,4 +11,6 @@ public interface BeerService {
     BeerDto getBeerById(UUID beerId);
 
     BeerDto saveNewBeer(BeerDto beerDto);
+
+    void updateBeer(UUID beerId, BeerDto beerDto);
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -40,4 +40,12 @@ public class BeerController {
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto){
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
 }
