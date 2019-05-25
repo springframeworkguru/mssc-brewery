5165769711227b85493cf3ddd844e9cadfe99f45
John Thompson <john@springframework.guru>
2019-05-25 16:44:26 -0400

    adding mapstruct example

---
+++ src/main/java/guru/springframework/msscbrewery/domain/Beer.java
@@ -0,0 +1,23 @@
+package guru.springframework.msscbrewery.domain;
+
+import guru.springframework.msscbrewery.web.model.v2.BeerStyleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-05-25.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+    private UUID id;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+}
