a817257a3cb2dde65b4561c1ca0e01a7dd26e2e3
John Thompson <john@springframework.guru>
2019-05-25 17:06:46 -0400

    adding mapstruct customer assignment

---
+++ src/main/java/guru/springframework/msscbrewery/web/mappers/CustomerMapper.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import guru.springframework.msscbrewery.domain.Customer;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 2019-05-25.
+ */
+@Mapper
+public interface CustomerMapper {
+
+    Customer customerDtoToCustomer(CustomerDto dto);
+
+    CustomerDto customerToCustomerDto(Customer customer);
+}
