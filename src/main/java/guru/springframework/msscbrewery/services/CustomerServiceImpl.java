df5c6b6ebb3b094683a0d91a8f2b07f82e28e95e
Kamil <qkiz288@gmail.com>
2020-07-06 11:32:51 +0200

    implemented POST, PUT, DELETE for CustomerDto

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -13,5 +15,23 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(UUID.randomUUID())
                 .name("Clark Kent")
                 .build();
+    }
+
+    @Override
+    public CustomerDto save(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("Jack")
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        log.info(String.format("Updating customer with id = %s", customerId));
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+        log.info(String.format("Deleting customer with id = %s", customerId));
     }
 }
