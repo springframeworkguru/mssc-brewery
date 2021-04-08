e17cfc64288ca60bc6e5e3b016f27dc3cf4a026d
cliffordbechtel <cliffordbechtel@cliffords-mini>
2021-04-07 23:12:52 -0400

    Create customer dto, controller, service

---
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbrewery.services;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public CustomerDto getCustomerById(UUID customerId) {
+		return CustomerDto.builder().id(UUID.randomUUID())
+                .customerName("Cigna")
+                .build();
+	}
+
+}
---
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbrewery.web.model;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDto {
+	
+	private UUID id;
+    private String customerName;
+}
