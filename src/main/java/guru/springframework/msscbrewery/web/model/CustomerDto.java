c78396b9242f57845f882ec83bedc2de2701d60d
Yash Bhardwaj <33004136+yashbhardwaj26@users.noreply.github.com>
2022-09-06 16:08:56 +0530

    Create CustomerDto.java

---
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -0,0 +1,15 @@
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Customer {
+	private UUID id;
+	private String name;
+
+}
