811e0c465486f63ea51a77c60209bde4b20af492
greg.langham <greg.langham@messagegears.com>
2023-01-30 11:25:35 -0600

    added v2

--- src/main/java/guru/springframework/msscbrewery/services/v2/BeerServiceV2Impl.java
+++ src/main/java/guru/springframework/msscbrewery/services/v2/BeerServiceV2Impl.java
@@ -3,7 +3,9 @@ package guru.springframework.msscbrewery.services.v2;
 import java.util.UUID;
 
 import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BeerServiceV2Impl implements BeerServiceV2 {
     @Override
     public BeerDtoV2 getBeerById(final UUID beerId) {
