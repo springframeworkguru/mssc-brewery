bd022666666718da91b0175ff22ece2a8c9de601
JanVDL1975 <86647408+JanVDL1975@users.noreply.github.com>
2023-07-11 18:24:32 +0200

    Update MsscBreweryApplication.java
    For testing

--- src/main/java/com/springframework/guru/msscbrewery/MsscBreweryApplication.java
+++ src/main/java/com/springframework/guru/msscbrewery/MsscBreweryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsscBreweryApplication {
-
+// Adding some changes to master. Simulating remote changes.
 	public static void main(String[] args) {
 		SpringApplication.run(MsscBreweryApplication.class, args);
 	}
